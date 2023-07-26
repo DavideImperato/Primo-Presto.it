9b8607fe3a7eb44d1f2a2dc376e97890065f156e
Davide Imperato <davideimperato95@gmail.com>
2023-07-26 22:56:44 +0200

    Progetto prima parte

---
+++ main.js
@@ -0,0 +1,31 @@
+
+let navbar = document.querySelector(".navbar");
+let titleMain = document.querySelector(".due");
+let navitem = document.querySelectorAll(".nav-link")
+
+
+window.addEventListener("scroll",()=>{
+    let scrollPage = window.scrollY;
+
+    if(scrollPage > 0){
+        navbar.classList.remove("m-4")
+        navbar.classList.add("navbarBlur")
+        navbar.style.backdropFilter= "blur(9px) saturate(0.8)"
+        
+
+
+
+    }else{
+        navbar.classList.add("m-4")
+        navbar.classList.remove("navbarBlur")
+
+    }
+     
+
+
+    
+
+})
+
+
+
