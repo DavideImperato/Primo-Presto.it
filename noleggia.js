d676e4d5affc3e01bf2156523c354013f4483a6d
Davide Imperato <davideimperato95@gmail.com>
2023-08-01 19:08:50 +0200

    aggiunti filtri per categoria,prezzo e parola, ed infine aggiunta la possibilita di ordinare in crescente o decrescente tramite un bottone

--- noleggia.js
+++ noleggia.js
@@ -1,6 +1,9 @@
 
 let categoriesWrepper = document.querySelector("#categoriesWrepper");
 let cardWrapper = document.querySelector("#cardWrapper")
+let sortBy = document.querySelector("#sortBy")
+
+
 
 
 
@@ -26,8 +29,6 @@ fetch('./articoli.json').then((response)=>response.json()).then((data)=>{
 
     }
     let uniqueCategories = []
-    
-    
     setCategory()
     
     
@@ -36,34 +37,42 @@ fetch('./articoli.json').then((response)=>response.json()).then((data)=>{
         let div = document.createElement("div")
         div.classList.add("form-check")
         div.innerHTML=`
-        <input class="form-check-input" type="checkbox" name="category" id="flexRadioDefault1">
+        <input class="form-check-input" type="radio" name="category" id="${category}">
         <label class="form-check-label" for="flexRadioDefault1">
           ${category}
         </label>
         `
         categoriesWrepper.appendChild(div);
-
+        
 
 
         
     })
+    
 
+    function createArticoli(array,condizione)  {
 
-    function createArticoli() {
-        let Products = data.map((annuncio)=> annuncio);
-        console.log(Products);
-        Products.forEach((prodotto)=>{
+        if(condizione!="crescente"){
+            array.sort((a,b)=>b.price - a.price)
+        }else{
+            array.sort((a,b)=>a.price - b.price)
+        }
+        
+            
+        cardWrapper.innerHTML=""
+
+        array.forEach((array)=>{
             let div = document.createElement("div")
             div.classList.add("card","cardArticle","m-3")
             div.innerHTML=`
-            <img src="${prodotto.image}" class="imageCoverArticle" alt="foto${prodotto.name}">
+            <img src="${array.image}" class="imageCoverArticle" alt="foto${array.name}">
             <div class="card-body">
-              <h5 class="card-title">${prodotto.name}</h5>
-              <p class="card-text"> Categoria: <br>${prodotto.category}</p>
+              <h5 class="card-title">${array.name}</h5>
+              <p class="card-text"> Categoria: <br>${array.category}</p>
               <div class="row align-items-center justify-content-around">
                 <div class="col-6 text-center my-auto">
                     <p class="p-0 m-0">a partire da</p>
-                    <h3>${prodotto.price}€</h3>
+                    <h3>${array.price}€</h3>
                 </div>
                 <div class="col-6 ms-auto"><a href="#" class="btn btn-primary">Noleggia</a></div>
               </div>
@@ -72,11 +81,129 @@ fetch('./articoli.json').then((response)=>response.json()).then((data)=>{
             cardWrapper.appendChild(div)
         })
         
-    }
-
-    createArticoli()
+        
     
 
+    }
+
+    createArticoli(data,"crescente")
+
+
+    let radio = document.querySelectorAll(".form-check-input")
+
+     function filterByCategories (array){
+        
+        let checked = Array.from(radio).find((button)=> button.checked);
+        let Categoria=checked.id
+
+        let filtered = array.filter((annuncio)=> annuncio.category == Categoria)
+        
+
+        
+        if(Categoria!="all"){
+            setPriceInput(filtered);
+            return filtered;
+            
+        }else{
+            return array;
+        }
+        
+     }
+     
+
+   radio.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        globalFilter()
+    })
+   })
+
+    let priceInput = document.querySelector("#priceInput")
+    let priceNumber = document.querySelector("#priceNumber")
+    let wordInput = document.querySelector("#wordInput")
+
+
+   function setPriceInput(array){
+
+        let arrayprice= array.map(articolo => articolo.price);
+        let maxPrice = Math.max(...arrayprice);
+        let minPrice = Math.min(...arrayprice);
+        priceInput.max = maxPrice
+        priceInput.min = minPrice
+        priceInput.value = maxPrice
+        priceNumber.innerHTML=maxPrice     
+   }
+
+   setPriceInput(data)
+
+   priceInput.addEventListener("input",()=>{
+    priceNumber.innerHTML=priceInput.value
+    globalFilter()
+   })
+
+   function filterByPrice(array) {
+    let filtered = array.filter((annuncio) => +annuncio.price <= +priceInput.value)
+    
+    return filtered
+   }
+
+   
+   wordInput.addEventListener("input",()=>{
+    
+    globalFilter()
+   })
+
+   function filterbyWord(array) {
+    let filtered = array.filter((annuncio)=> annuncio.name.toLowerCase().includes(wordInput.value.toLowerCase()))
+    return filtered
+   }
+
+   function globalFilter(){
+
+    let filterCategory = filterByCategories(data)
+    let filterPrice = filterByPrice(filterCategory)
+    let filterWord = filterbyWord(filterPrice)
+
+     createArticoli(filterWord)
+     setorder(filterWord)
+
+
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+   let check=false;
+
+   sortBy.addEventListener("click",()=>{
+    globalFilter()
+    
+   })
+   function setorder (array){
+    if(check==false){
+        sortBy.classList.remove("bi-sort-numeric-down")
+        sortBy.classList.add("bi-sort-numeric-up")
+        createArticoli(array,"decrescente")
+        check=true;
+    }else if(check==true){
+        sortBy.classList.remove("bi-sort-numeric-up")
+        sortBy.classList.add("bi-sort-numeric-down")
+        createArticoli(array,"crescente")
+        check=false;
+    }
+   }
+   
 
 });
 
